bc826be389a0439a5c49e9b624a681d0438d2c32
M1steryO <pda1205@gmail.com>
2022-12-11 22:22:22 +0300

    flake-8

--- tarot/fortune/urls.py
+++ tarot/fortune/urls.py
@@ -1,6 +1,8 @@
-from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [
+from fortune.views import FortuneListView
 
+app_name = 'fortune'
+urlpatterns = [
+    path('', FortuneListView.as_view(), name="fortune_list")
 ]
--- tarot/fortune/views.py
+++ tarot/fortune/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+
+class FortuneListView(ListView):
+    # model = Fortune
+    template_name = 'fortune/fortune_detail.html'
+    context_object_name = 'item'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        return context
+
+    def get_queryset(self):
+        pass
+
+    def post(self, request, *args, **kwargs):
+        pass
