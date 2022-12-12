4e7e68390328951d10aebf5fd16e9c5a266ce7cd
Kate Petrova <2005kpetrova@gmail.com>
2022-12-12 19:44:12 +0300

    flake8

--- tarot/fortune/urls.py
+++ tarot/fortune/urls.py
@@ -5,5 +5,9 @@ from . import views
 app_name = 'fortune'
 urlpatterns = [
     path('', views.FortuneListView.as_view(), name="fortune_list"),
-    path('<int:pk>/', views.FortuneDetailView.as_view(), name='fortune_detail'),
+    path(
+        '<int:pk>/',
+        views.FortuneDetailView.as_view(),
+        name='fortune_detail'
+    ),
 ]
