1826ec95ad96f6f14f4afdb6e587349136bda535
Kate Petrova <2005kpetrova@gmail.com>
2022-12-21 20:24:20 +0300

    fixes #9

--- tarot/users/urls.py
+++ tarot/users/urls.py
@@ -7,7 +7,10 @@ app_name = 'users'
 
 urlpatterns = [
     path('login/',
-         auth_views.LoginView.as_view(template_name='users/login.html'),
+         auth_views.LoginView.as_view(
+             template_name='users/login.html',
+             redirect_authenticated_user=True
+         ),
          name='login'),
     path('logout/',
          auth_views.LogoutView.as_view(template_name='users/logout.html'),
