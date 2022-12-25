ce337fb2267fe26bc5075b0342c87570b6283561
Kate Petrova <2005kpetrova@gmail.com>
2022-12-25 22:54:14 +0300

    pep8

--- tarot/users/views.py
+++ tarot/users/views.py
@@ -8,7 +8,6 @@ from django.urls import reverse_lazy
 from django.views.generic import UpdateView
 from django.views.generic.edit import CreateView
 
-from coins.models import BankAccount
 from tarot import settings
 
 from .forms import AccountChangeForm, AccountCreationForm
